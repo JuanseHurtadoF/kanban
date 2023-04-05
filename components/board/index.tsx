f89d2192ea818d905ae3f395c999d5a98f2e6529
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-04-05 16:16:27 -0500

    layout working

--- components/board/index.tsx
+++ components/board/index.tsx
@@ -1,8 +1,20 @@
 import React, { FC } from "react";
 import styles from "./board.module.scss";
+import { data } from "@data";
+import { Column } from "@components";
 
 const Board: FC = () => {
-  return <div className={styles.container}></div>;
+  const columns = data.boards[0].columns;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.board}>
+      {columns.map(({name, tasks}) => {
+        return <Column key={name} name={name} cards={tasks} />;
+      })}
+      </div>
+    </div>
+  );
 };
 
 export default Board;
--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
 import { Sidebar, Nav } from "@components";
 import styles from "./layout.module.scss";
+import { Column } from "@components"; 
+import { data } from "@data";
+import Board from "@components/board";
 
 const Layout: FC = () => {
   return (
@@ -8,6 +11,7 @@ const Layout: FC = () => {
       <Sidebar />
       <div>
         <Nav />
+        <Board />
       </div>
     </div>
   );
