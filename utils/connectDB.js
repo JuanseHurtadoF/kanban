70b6e38f0dddfdb3564fa74a3a39cc4bfebe9644
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-05-05 15:01:23 -0500

    remove connectjs cached connection

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,26 +1,22 @@
 import mongoose from "mongoose";
 
-let cachedConnection = null;
 
 const connectDB = async (url) => {
-  if (cachedConnection && cachedConnection.readyState >= 1) {
-    // If the cached connection is ready, return without reconnecting
+  if (mongoose.connection.readyState >= 1) {
+    // If the connection is ready, return without reconnecting
     return;
   }
-
   if (!url) {
     console.error("Error: MongoDB URL is missing");
     return;
   }
   try {
-    const connection = await mongoose.connect(url, {
+    await mongoose.connect(url, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       maxPoolSize: 10,
     });
     console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-
-    cachedConnection = connection;
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
