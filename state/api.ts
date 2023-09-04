7cbcaa72a9ae2807bc61fab5544e2a0750ef2b3f
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2023-09-03 21:30:55 -0500

    api test

--- state/api.ts
+++ state/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api: any = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://kanban-teal.vercel.app//api",
+    baseUrl: "/api",
   }),
   tagTypes: ["Board", "Task"],
   endpoints: (build) => ({
