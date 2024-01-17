b40a93d0aa4e92cf99b30945e0dbe99f808e5af6
JuanseHurtadoF <juansebastianhurtadof@gmail.com>
2024-01-17 16:42:21 -0500

    card title is now editable

--- hooks/useEditTask.tsx
+++ hooks/useEditTask.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEditTaskMutation } from "state/api";
+import { editTaskLocal } from "state";
 
 const useEditTask = () => {
   const boardId = useSelector((state: any) => state.global.activeBoard._id);
@@ -10,6 +11,9 @@ const useEditTask = () => {
   const taskId = useSelector((state: any) => state.global.highlightedCard._id);
 
   const updateTask = async ({ name, prevName }) => {
+
+    dispatch(editTaskLocal({ newName: name }));
+
     const response = await editTask({
       title: name,
       taskId,
